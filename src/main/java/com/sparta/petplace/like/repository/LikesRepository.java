d4b0477ef4c146beca16717bf647eb9cec15f639
PyoChangYeong <game1425@naver.com>
2023-03-14 22:23:45 +0900

    #8 Fix: import 수정

--- src/main/java/com/sparta/petplace/like/repository/LikesRepository.java
+++ src/main/java/com/sparta/petplace/like/repository/LikesRepository.java
@@ -5,10 +5,13 @@ import com.sparta.petplace.member.entity.Member;
 import com.sparta.petplace.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     Optional<Likes> findByPostAndMember(Post post, Member member);
 
     Likes findByPostIdAndMember(Long post_id , Member member);
+
+
 }
