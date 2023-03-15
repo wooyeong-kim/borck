178aad9e461577d65ea0905c71cd18af55f512c1
civilcoy <joungcb06@gmail.com>
2023-03-15 18:52:34 +0900

    Merge branch 'dev' of https://github.com/TeamPetPlace/BE into feat/reviews
    * 'dev' of https://github.com/TeamPetPlace/BE:
      fix : 코드누락 수정
    
    # Conflicts:
    #	src/main/java/com/sparta/petplace/exception/enumclass/Error.java
    #	src/main/java/com/sparta/petplace/review/repository/ReviewRepository.java

--- src/main/java/com/sparta/petplace/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/petplace/review/repository/ReviewRepository.java
@@ -4,10 +4,11 @@ import com.sparta.petplace.member.entity.Member;
 import com.sparta.petplace.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     void deleteByPostId(Long postId);
     Optional<Review> findByIdAndMember(Long id, Member member);
-
+    Optional<List<Review>> findAllByMemberId(Long memberId);
 }
